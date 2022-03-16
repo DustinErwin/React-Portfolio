67bb78a33cbd7821a807cbcd9409afbc91f6c757
Galahad <derwin499@gmail.com>
2022-03-15 20:29:39 -0400

    updates for images

---
+++ client/build/precache-manifest.9856c48ddc44a824f34a832bc5ca8711.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e3f86b2daca7437f4ebc968c05b93e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "604e94f50d0d92414350",
+    "url": "/static/css/main.f198f308.chunk.css"
+  },
+  {
+    "revision": "941e1c6b258e60bf328c",
+    "url": "/static/js/2.6d048eef.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6d048eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604e94f50d0d92414350",
+    "url": "/static/js/main.e98c0034.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "f1d600084427d39b9cfcc6140be8d455",
+    "url": "/static/media/20842368.f1d60008.jpg"
+  },
+  {
+    "revision": "867b27ba0bb956d0077577577b74b8c5",
+    "url": "/static/media/DevFitReact.867b27ba.png"
+  },
+  {
+    "revision": "16741188aa8b074ca24b859cdff28d71",
+    "url": "/static/media/couchSurf.16741188.png"
+  },
+  {
+    "revision": "e3d3489dea8168af3c6502e2bc9ad918",
+    "url": "/static/media/dragonlogo.e3d3489d.png"
+  },
+  {
+    "revision": "1ffcc36a4a82636b74c869f2b1ae8691",
+    "url": "/static/media/dustinBlink.1ffcc36a.png"
+  },
+  {
+    "revision": "22af960785c4015770b5f7bd04d412f9",
+    "url": "/static/media/dustinOpen.22af9607.png"
+  },
+  {
+    "revision": "9a971f6337a2d532aebf27a596cde408",
+    "url": "/static/media/empDirect.9a971f63.png"
+  },
+  {
+    "revision": "1ef77b31fbe00b415714923f7d8510d3",
+    "url": "/static/media/linkedIn.1ef77b31.jpg"
+  },
+  {
+    "revision": "191eade5a6858cc818d8f7410d8548ea",
+    "url": "/static/media/mongodb.191eade5.png"
+  },
+  {
+    "revision": "38821fb26887a56e79cd9f73f5f06e41",
+    "url": "/static/media/picMe.38821fb2.png"
+  },
+  {
+    "revision": "0da46d38ffb458585905dee132409b94",
+    "url": "/static/media/react.0da46d38.png"
+  }
+]);
